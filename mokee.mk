8fe4507d6e4c7b4d6e98b91c9975d63e7404b05e
martincz <martincz@mokeedev.com>
2016-02-25 12:41:17 +0800

    ls990: mokee bringup

--- mokee.mk
+++ mokee.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/lge/ls990/full_ls990.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := cm_ls990
+PRODUCT_NAME := mk_ls990
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
